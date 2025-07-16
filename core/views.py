30b35f13ecd8b88f4bff845bc03688ee765d2c0f
C2002G <claudiogabriel2002@gmail.com>
2025-07-16 15:23:00 -0300

    login e logout criado

--- core/views.py
+++ core/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render,redirect
 from core.models import Evento
-# Create your views here.
-# def index(request):
-#     return redirect('/agenda/')
-def lista_eventos(request):
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
-    evento = Evento.objects.all
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request,):
+    if request.POST:
+        username= request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(request, username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+    else:
+        redirect('/')
+@login_required(login_url='/login/')
+def lista_eventos(request):
+    usario = request.user
+    evento = Evento.objects.filter(usuario=usario)
     dados = {'eventos': evento}
     return render (request, 'agenda.html', dados)
